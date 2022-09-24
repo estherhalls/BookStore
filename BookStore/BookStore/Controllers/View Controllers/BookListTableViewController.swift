c0dcd6190c1f28cc13e449aca9aa3307079b861d
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-09-23 18:52:15 -0600

    detail screen created

--- BookStore/BookStore/Controllers/View Controllers/BookListTableViewController.swift
+++ BookStore/BookStore/Controllers/View Controllers/BookListTableViewController.swift
@@ -18,6 +18,9 @@ class BookListTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
 
     // MARK: - Table view data source
 
